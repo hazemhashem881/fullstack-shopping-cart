fadf17c1e0f06d4c3d6209f3177ed33cb7affcfb
hazem hashem <hazemhashem100@gamil.com>
2023-12-30 01:32:59 +0200

    db

--- frontend/src/api/catalog.ts
+++
@@ -1,5 +0,0 @@
-import http from './index';
-
-const PREFIX = '/catalog';
-
-export const getCatalog = () => http.get(PREFIX);
--- frontend/src/api/user.ts
+++
@@ -1,7 +0,0 @@
-import http from './index';
-
-const PREFIX = '/user';
-
-export const getUser = () => http.get(PREFIX);
-
-export const editUser = (data: Record<string, any>) => http.put(PREFIX, data);
